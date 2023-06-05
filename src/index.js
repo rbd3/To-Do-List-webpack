64ad7f9edfdbca445fcc07f7ae17644242ef8999
rbd3 <rabedesanaa@gmail.com>
2023-06-05 17:46:51 +0300

    Fix identation error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-  const element = document.createElement('div');  
+  const element = document.createElement('div');
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
   return element;
 }
 
